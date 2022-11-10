40aa9b29b860b85368dcc4ceb035e3d32b72637b
ddawg776 <ddawg776@gmail.com>
2022-11-10 16:32:44 -0600

    Set up express and other thing

--- swoleCode/db/index.js
+++ swoleCode/db/index.js
@@ -1,1 +1,10 @@
 // This is where we will be building pg.Client instance
+
+const pg = require('pg');
+
+const  client  = new pg.Client('postgres://localhost:3000/fitness-dev')
+
+
+module.exports - {
+    client
+}
