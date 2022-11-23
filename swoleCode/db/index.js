1ff43346ed18194ea8f994575024ffca01c3cfa5
John Tacuri <johnsteven1231@gmail.com>
2022-11-23 15:30:36 -0500

    finally done

--- swoleCode/db/index.js
+++ swoleCode/db/index.js
@@ -1,8 +1,9 @@
 // This is where we will be building pg.Client instance
+require('dotenv').config()
 
 const pg = require('pg');
 
-const  client  = new pg.Client('postgres://localhost:5432/fitness-dev')
+const client = new pg.Client(process.env.DB_URL || 'postgres://localhost:5432/fitness-dev')
 
 
 module.exports = {
